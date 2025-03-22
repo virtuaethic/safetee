9e68619cc1bc9f0255d513cab29f62a45900adc3
Andrei Bratuhin <andrei.bratuhin@gmail.com>
2025-03-22 20:06:36 +0300

    add fonts

---
+++ safetee-website/app/fonts.ts
@@ -0,0 +1,115 @@
+import localFont from 'next/font/local'
+
+export const bertholdAkzidenz = localFont({
+  src: [
+    {
+      path: '../public/fonts/Berthold-Akzidenz-Grotesk-Extended.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Berthold-Akzidenz-Grotesk-Extended.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Berthold-Akzidenz-Grotesk-Extended.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Berthold-Akzidenz-Grotesk-Extended.eot',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Berthold-Akzidenz-Grotesk-Extended.svg',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Berthold-Akzidenz-Grotesk-Extended-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Berthold-Akzidenz-Grotesk-Extended-Bold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Berthold-Akzidenz-Grotesk-Extended-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Berthold-Akzidenz-Grotesk-Extended-Bold.eot',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Berthold-Akzidenz-Grotesk-Extended-Bold.svg',
+      weight: '700',
+      style: 'normal',
+    }
+  ],
+  variable: '--font-berthold-akzidenz',
+  display: 'swap',
+})
+
+export const avantgarde = localFont({
+  src: [
+    {
+      path: '../public/fonts/Avantgarde.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Avantgarde.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Avantgarde.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Avantgarde.eot',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Avantgarde.svg',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Avantgarde-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Avantgarde-Bold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Avantgarde-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Avantgarde-Bold.eot',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Avantgarde-Bold.svg',
+      weight: '700',
+      style: 'normal',
+    }
+  ],
+  variable: '--font-avantgarde',
+  display: 'swap',
+}) 
